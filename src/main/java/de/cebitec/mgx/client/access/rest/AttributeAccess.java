71e1285aa284845805296d7e254fb585ba512f8f
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-01-22 17:13:17 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@411 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- src/main/java/de/cebitec/mgx/client/access/rest/AttributeAccess.java
+++ src/main/java/de/cebitec/mgx/client/access/rest/AttributeAccess.java
@@ -6,8 +6,6 @@ import de.cebitec.mgx.dto.dto.AttributeCount;
 import de.cebitec.mgx.dto.dto.AttributeDTO;
 import de.cebitec.mgx.dto.dto.AttributeDTOList;
 import de.cebitec.mgx.dto.dto.AttributeDistribution;
-import de.cebitec.mgx.dto.dto.MGXString;
-import de.cebitec.mgx.dto.dto.MGXStringList;
 import java.util.Collection;
 import java.util.List;
 
@@ -17,34 +15,29 @@ import java.util.List;
  */
 public class AttributeAccess extends AccessBase<AttributeDTO, AttributeDTOList> {
 
-    public Collection<MGXString> listTypes() throws MGXServerException {
-        return get("/Attribute/listTypes/", MGXStringList.class).getStringList();
+//    public Collection<MGXString> listTypes() throws MGXServerException {
+//        return get("/Attribute/listTypes/", MGXStringList.class).getStringList();
+//    }
+//
+//    public Collection<MGXString> listTypesByJob(Long jobId) throws MGXServerException {
+//        return get("/Attribute/listTypesByJob/" + jobId, MGXStringList.class).getStringList();
+//    }
+
+    public List<AttributeDTO> BySeqRun(Long seqrunId) throws MGXServerException {
+        return get("/Attribute/BySeqRun/" + seqrunId, AttributeDTOList.class).getAttributeList();
     }
 
-    public Collection<MGXString> listTypesByJob(Long jobId) throws MGXServerException {
-        return get("/Attribute/listTypesByJob/" + jobId, MGXStringList.class).getStringList();
+    public List<AttributeCount> getDistribution(Long attr_id) throws MGXServerException {
+        return get("/Attribute/getDistribution/" + attr_id, AttributeDistribution.class).getAttributecountList();
     }
 
-    public Collection<MGXString> listTypesBySeqRun(Long seqrunId) throws MGXServerException {
-        return get("/Attribute/listTypesBySeqRun/" + seqrunId, MGXStringList.class).getStringList();
-    }
-
-    public List<AttributeCount> getDistribution(String attributeName, Long jobId, List<Long> seqrun_ids) throws MGXServerException {
-        String uri = new StringBuilder("/Attribute/getDistribution/")
-                .append(attributeName).append("/")
-                .append(jobId.toString()).append("/")
-                .append(join(seqrun_ids, ","))
-                .toString();
-        return get(uri, AttributeDistribution.class).getAttributecountList();
-    }
-
-    public List<AttributeCount> getDistributionByRuns(String attributeName, List<Long> seqrun_ids) throws MGXServerException {
-        String uri = new StringBuilder("/Attribute/getDistributionByRuns/")
-                .append(attributeName).append("/")
-                .append(join(seqrun_ids, ","))
-                .toString();
-        return get(uri, AttributeDistribution.class).getAttributecountList();
-    }
+//    public List<AttributeCount> getDistributionByRuns(String attributeName, List<Long> seqrun_ids) throws MGXServerException {
+//        String uri = new StringBuilder("/Attribute/getDistributionByRuns/")
+//                .append(attributeName).append("/")
+//                .append(join(seqrun_ids, ","))
+//                .toString();
+//        return get(uri, AttributeDistribution.class).getAttributecountList();
+//    }
 
     @Override
     public AttributeDTO fetch(Long id) throws MGXServerException, MGXClientException {
@@ -58,16 +51,16 @@ public class AttributeAccess extends AccessBase<AttributeDTO, AttributeDTOList> 
 
     @Override
     public Long create(AttributeDTO t) throws MGXServerException, MGXClientException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Not supported.");
     }
 
     @Override
     public void update(AttributeDTO t) throws MGXServerException, MGXClientException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Not supported.");
     }
 
     @Override
     public void delete(Long id) throws MGXServerException, MGXClientException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Not supported.");
     }
 }
