6d3094a73b9cdc68c2c2f60e60b1018160a7c5f4
Peter Belmann <pbelmann@CeBiTec.Uni-Bielefeld.DE>
2012-10-18 07:52:56 +0000

    XML bei getAvailableParameters wird nun validiert.
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@937 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- src/main/java/de/cebitec/mgx/client/access/rest/ToolAccess.java
+++ src/main/java/de/cebitec/mgx/client/access/rest/ToolAccess.java
@@ -1,5 +1,7 @@
 package de.cebitec.mgx.client.access.rest;
 
+import de.cebitec.mgx.client.access.rest.util.XMLValidator;
+import java.util.logging.Logger;
 import de.cebitec.mgx.client.exception.MGXClientException;
 import de.cebitec.mgx.client.exception.MGXServerException;
 import de.cebitec.mgx.dto.dto.JobParameterDTO;
@@ -7,7 +9,11 @@ import de.cebitec.mgx.dto.dto.JobParameterListDTO;
 import de.cebitec.mgx.dto.dto.MGXLong;
 import de.cebitec.mgx.dto.dto.ToolDTO;
 import de.cebitec.mgx.dto.dto.ToolDTOList;
+import java.io.IOException;
 import java.util.Collection;
+import java.util.logging.Level;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -15,11 +21,31 @@ import java.util.Collection;
  */
 public class ToolAccess extends AccessBase<ToolDTO, ToolDTOList> {
 
+    private static final Logger LOGGER = Logger.getAnonymousLogger();
+
     public Iterable<JobParameterDTO> getAvailableParameters(long tool_id, boolean isGlobal) throws MGXServerException {
         return get("/Tool/getAvailableParameters/" + tool_id + "/" + isGlobal, JobParameterListDTO.class).getParameterList();
     }
 
     public Iterable<JobParameterDTO> getAvailableParameters(ToolDTO dto) throws MGXServerException {
+        //TODO: Abfragen, ob xml valide..
+
+        XMLValidator validator = new XMLValidator();
+        try {
+            if (!validator.isValid(dto.getXml())) {
+                throw new MGXServerException("XML is not Valid");
+            }
+        } catch (SAXException ex) {
+            Logger.getLogger(ToolAccess.class.getName()).log(Level.SEVERE, null, ex);
+            throw new MGXServerException("XML is not valid: " + ex.getMessage());
+        } catch (IOException ex) {
+            Logger.getLogger(ToolAccess.class.getName()).log(Level.SEVERE, null, ex);
+            throw new MGXServerException("XML is not valid: " + ex.getMessage());
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(ToolAccess.class.getName()).log(Level.SEVERE, null, ex);
+            throw new MGXServerException("XML is not valid: " + ex.getMessage());
+        }
+
         return put("/Tool/getAvailableParameters/", dto, JobParameterListDTO.class).getParameterList();
     }
 
