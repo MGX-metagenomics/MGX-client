3eef23c9d8b39ab69cccbde6893430bedf0e83e9
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-08-28 10:59:13 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1462 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- src/main/java/de/cebitec/mgx/client/access/rest/ReferenceAccess.java
+++ src/main/java/de/cebitec/mgx/client/access/rest/ReferenceAccess.java
@@ -1,45 +1,41 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package de.cebitec.mgx.client.access.rest;
 
-import static de.cebitec.mgx.client.access.rest.AccessBase.r;
 import de.cebitec.mgx.client.exception.MGXClientException;
 import de.cebitec.mgx.client.exception.MGXServerException;
-import de.cebitec.mgx.dto.dto;
+import de.cebitec.mgx.dto.dto.ReferenceDTO;
+import de.cebitec.mgx.dto.dto.ReferenceDTOList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.UUID;
 
 /**
  *
  * @author belmann
  */
-public class ReferenceAccess extends AccessBase<dto.ReferenceDTO, dto.ReferenceDTOList>{
+public class ReferenceAccess extends AccessBase<ReferenceDTO, ReferenceDTOList>{
      
     @Override
-    public Iterator<dto.ReferenceDTO> fetchall() throws MGXServerException, MGXClientException {
-        return fetchlist(dto.ReferenceDTOList.class).getReferenceList().iterator();
+    public Iterator<ReferenceDTO> fetchall() throws MGXServerException, MGXClientException {
+        return fetchlist(ReferenceDTOList.class).getReferenceList().iterator();
     }
 
     @Override
-    public long create(dto.ReferenceDTO sr) throws MGXServerException, MGXClientException {
-        return super.create(sr, dto.ReferenceDTO.class);
+    public long create(ReferenceDTO sr) throws MGXServerException, MGXClientException {
+        return super.create(sr, ReferenceDTO.class);
     }
 
     @Override
-    public void update(dto.ReferenceDTO d) throws MGXServerException, MGXClientException {
-        super.update(d, dto.ReferenceDTO.class);
+    public void update(ReferenceDTO d) throws MGXServerException, MGXClientException {
+        super.update(d, ReferenceDTO.class);
     }
 
     @Override
     public UUID delete(long id) throws MGXServerException, MGXClientException {
-        return super.delete(id, dto.ReferenceDTO.class);
+        return super.delete(id, ReferenceDTO.class);
     }
 
     @Override
-    public dto.ReferenceDTO fetch(long id) throws MGXServerException, MGXClientException {
-        return super.fetch(id, dto.ReferenceDTO.class);    
+    public ReferenceDTO fetch(long id) throws MGXServerException, MGXClientException {
+        return super.fetch(id, ReferenceDTO.class);    
     }
 }
