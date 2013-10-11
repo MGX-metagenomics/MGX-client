377c922545b201057660419fe5e408f47a5293f7
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-10-11 11:48:22 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1588 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- src/main/java/de/cebitec/mgx/client/access/rest/ReferenceAccess.java
+++ src/main/java/de/cebitec/mgx/client/access/rest/ReferenceAccess.java
@@ -4,6 +4,7 @@ import de.cebitec.mgx.client.datatransfer.ReferenceUploader;
 import de.cebitec.mgx.client.exception.MGXClientException;
 import de.cebitec.mgx.client.exception.MGXServerException;
 import de.cebitec.mgx.dto.dto.MGXLong;
+import de.cebitec.mgx.dto.dto.MGXString;
 import de.cebitec.mgx.dto.dto.ReferenceDTO;
 import de.cebitec.mgx.dto.dto.ReferenceDTOList;
 import de.cebitec.mgx.dto.dto.RegionDTO;
@@ -36,7 +37,7 @@ public class ReferenceAccess extends AccessBase<ReferenceDTO, ReferenceDTOList> 
     public long installGlobalReference(long id) throws MGXServerException {
         return get("/Reference/installGlobalReference/" + id, MGXLong.class).getValue();
     }
-    
+
     @Override
     public UUID delete(long id) throws MGXServerException, MGXClientException {
         return super.delete(id, ReferenceDTO.class);
@@ -50,12 +51,15 @@ public class ReferenceAccess extends AccessBase<ReferenceDTO, ReferenceDTOList> 
     public Iterator<ReferenceDTO> listGlobalReferences() throws MGXServerException {
         return get("/Reference/listGlobalReferences", ReferenceDTOList.class).getReferenceList().iterator();
     }
-    
-    
+
     public Iterator<RegionDTO> byReferenceInterval(long id, int from, int to) throws MGXClientException, MGXServerException {
         return get("/Reference/byReferenceInterval/" + id + "/" + from + "/" + to, RegionDTOList.class).getRegionList().iterator();
     }
-    
+
+    public String getSequence(long id, int from, int to) throws MGXClientException, MGXServerException {
+        return get("/Reference/getSequence/" + id + "/" + from + "/" + to, MGXString.class).getValue();
+    }
+
     public ReferenceUploader createUploader(File localFile) {
         return new ReferenceUploader(getWebResource(), localFile);
     }
