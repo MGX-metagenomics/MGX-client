b46ec822faa7d447de57178b04ee6a63176760e6
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-01-13 15:40:27 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1736 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- src/main/java/de/cebitec/mgx/client/access/rest/FileAccess.java
+++ src/main/java/de/cebitec/mgx/client/access/rest/FileAccess.java
@@ -27,11 +27,10 @@ public class FileAccess extends AccessBase<FileDTO, FileDTOList> {
         throw new UnsupportedOperationException("Not supported.");
     }
 
-    public boolean delete(FileDTO dto) throws MGXServerException, MGXClientException {
+    public UUID delete(FileDTO dto) throws MGXServerException, MGXClientException {
         String path = dto.getName().replace("/", "|");
         String resolve = r.resolve(FileDTO.class, "delete");
-        this.delete(resolve + path);
-        return true;
+        return UUID.fromString(this.delete(resolve + path));
     }
 
     @Override
