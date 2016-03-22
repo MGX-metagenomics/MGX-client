ed43d01a76e1587059809ea5319371d589aeb2c3
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-03-22 15:41:49 +0100

    add master test class

---
+++ src/test/java/de/cebitec/mgx/client/MGXDTOMasterTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.client;
+
+import de.cebitec.mgx.client.mgxtestclient.TestMaster;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sj
+ */
+public class MGXDTOMasterTest {
+
+    public MGXDTOMasterTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+//    @Test
+//    public void testEquals() {
+//        System.out.println("equals");
+//        MGXDTOMaster m1 = TestMaster.getPrivate("MGX_deNBI1");
+//        MGXDTOMaster m2 = TestMaster.getPrivate("MGX_deNBI4");
+//        assertNotNull(m1);
+//        assertNotNull(m2);
+//        assertNotEquals(m1, m2);
+//    }
+
+}
