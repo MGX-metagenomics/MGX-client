210516b8c34cbd8620a2ca5c0dc6f69f772d9657
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-04-29 07:55:14 +0200

    rework tool creation; TODO: add unit tests

--- src/main/java/de/cebitec/mgx/client/access/rest/ToolAccess.java
+++ src/main/java/de/cebitec/mgx/client/access/rest/ToolAccess.java
@@ -22,52 +22,61 @@ public class ToolAccess extends AccessBase<ToolDTO, ToolDTOList> {
     public ToolAccess(RESTAccessI restAccess) {
         super(restAccess);
     }
-    
+
     public Iterable<JobParameterDTO> getAvailableParameters(long tool_id, boolean isGlobal) throws MGXServerException {
         return get(JobParameterListDTO.class, "Tool", "getAvailableParameters", String.valueOf(tool_id), String.valueOf(isGlobal)).getParameterList();
     }
-    
-    public Iterable<JobParameterDTO> getAvailableParameters(ToolDTO dto) throws MGXServerException, MGXClientException {
+
+    public Iterable<JobParameterDTO> getAvailableParameters(String toolXml) throws MGXClientException, MGXServerException {
         XMLValidator validator = new XMLValidator();
-        if (!validator.isValid(dto.getXml())) {
-            throw new MGXClientException("Invalid tool file");
+        if (!validator.isValid(toolXml)) {
+            throw new MGXClientException("Invalid tool XML data");
         }
+        MGXString dto = MGXString.newBuilder().setValue(toolXml).build();
         return put(dto, JobParameterListDTO.class, "Tool", "getAvailableParameters").getParameterList();
     }
-    
+
+//    public Iterable<JobParameterDTO> getAvailableParameters(ToolDTO dto) throws MGXServerException, MGXClientException {
+//        XMLValidator validator = new XMLValidator();
+//        if (!validator.isValid(dto.getXml())) {
+//            throw new MGXClientException("Invalid tool file");
+//        }
+//        return put(dto, JobParameterListDTO.class, "Tool", "getAvailableParameters").getParameterList();
+//    }
+
     @Override
     public Iterator<ToolDTO> fetchall() throws MGXServerException, MGXClientException {
         return fetchlist(ToolDTOList.class).getToolList().iterator();
     }
-    
+
     public Iterator<ToolDTO> listGlobalTools() throws MGXServerException {
         return get(ToolDTOList.class, "Tool", "listGlobalTools").getToolList().iterator();
     }
-    
+
     public long installGlobalTool(long global_id) throws MGXServerException {
         return get(MGXLong.class, "Tool", "installGlobalTool", String.valueOf(global_id)).getValue();
     }
-    
+
     @Override
     public UUID delete(long id) throws MGXServerException, MGXClientException {
         return super.delete(id, ToolDTO.class);
     }
-    
+
     @Override
     public ToolDTO fetch(long id) throws MGXServerException, MGXClientException {
         return super.fetch(id, ToolDTO.class);
     }
-    
+
     @Override
     public long create(ToolDTO t) throws MGXServerException, MGXClientException {
         return super.create(t, ToolDTO.class);
     }
-    
+
     @Override
     public void update(ToolDTO t) throws MGXServerException, MGXClientException {
         super.update(t, ToolDTO.class);
     }
-    
+
     public ToolDTO ByJob(long job_id) throws MGXServerException, MGXClientException {
         return get(ToolDTO.class, r.resolve(ToolDTO.class, "byJob", String.valueOf(job_id)));
     }
@@ -75,4 +84,5 @@ public class ToolAccess extends AccessBase<ToolDTO, ToolDTOList> {
     public String getXMLDefinition(long tool_id) throws MGXServerException, MGXClientException {
         return get(MGXString.class, r.resolve(ToolDTO.class, "getXML", String.valueOf(tool_id))).getValue();
     }
+
 }
