e600aaf0badec3392e7262863d5e56a888574359
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2019-08-21 15:42:04 +0200

    implement gene#getSequence

--- src/main/java/de/cebitec/mgx/client/access/rest/GeneAccess.java
+++ src/main/java/de/cebitec/mgx/client/access/rest/GeneAccess.java
@@ -1,9 +1,11 @@
 package de.cebitec.mgx.client.access.rest;
 
 import de.cebitec.gpms.rest.RESTAccessI;
+import de.cebitec.mgx.client.exception.MGXClientException;
 import de.cebitec.mgx.client.exception.MGXDTOException;
 import de.cebitec.mgx.dto.dto.GeneDTO;
 import de.cebitec.mgx.dto.dto.GeneDTOList;
+import de.cebitec.mgx.dto.dto.MGXStringList;
 import java.util.Iterator;
 import java.util.UUID;
 
@@ -44,5 +46,10 @@ public class GeneAccess extends AccessBase<GeneDTO, GeneDTOList> {
 
     public Iterator<GeneDTO> byContig(long contig_id) throws MGXDTOException {
         return get(GeneDTOList.class, r.resolve(GeneDTOList.class, "byContig", String.valueOf(contig_id))).getGeneList().iterator();
+    }
+
+    public String[] getSequence(long gene_id) throws MGXDTOException {
+        MGXStringList get = get(MGXStringList.class, r.resolve(MGXStringList.class, "getSequence", String.valueOf(gene_id)));
+        return new String[]{get.getString(0).getValue(), get.getString(1).getValue()};
     }
 }
